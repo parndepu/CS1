10ef3be0ece77544ab9f6315ab28c6a763d310db
Parn <j.suphanut@gmail.com>
2019-04-29 21:14:58 -0400

    Create Lab12_Vectors.cpp

---
+++ Lab12_Vectors.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+int main() 
+{
+    std::vector<double> myNumbers;
+    double number;
+    char opt;
+    
+    std::cout << "enter operation [a/r/q] and number: ";
+    std::cin >> opt;
+
+    while (opt != 'q') {
+        
+        std::cin >> number;
+        std::vector<double>::iterator it;
+        
+        if (opt == 'a' ) {
+            it = find(myNumbers.begin(), myNumbers.end(), number);
+            
+            // Bonus: detect odd numbers
+            if (it == myNumbers.end() && (int)number % 2 != 0) {
+                myNumbers.push_back(number);
+            }
+        }
+        if (opt == 'r') {
+            it = find(myNumbers.begin(), myNumbers.end(), number);
+            
+            // Remove number if exists
+            if (it != myNumbers.end()){
+                myNumbers.erase(it);
+            }
+        }
+        
+        // Output all numbers
+        std::cout << "your numbers: ";
+        for (int i = 0; i < myNumbers.size(); i++){
+            std::cout << myNumbers[i] << " ";
+        }
+        
+        std::cout << std::endl;
+        std::cout << "enter operation [a/r/q] and numbers: ";
+        std::cin >> opt;
+    }
+}
